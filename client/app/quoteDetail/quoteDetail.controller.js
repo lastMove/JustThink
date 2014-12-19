2a9eef1511a81cd322464d5d4c3dd9f521a6103a
freesaoul <anthonycamara@MacBook-Pro-de-Anthony-3.local>
2014-12-19 16:23:41 +0100

    update

--- client/app/quoteDetail/quoteDetail.controller.js
+++ client/app/quoteDetail/quoteDetail.controller.js
@@ -4,10 +4,10 @@ angular.module('justThinkApp')
   .controller('QuoteDetailCtrl', function ($scope, $param) {
     $scope.message = 'Hello';
     var id = param.id;
-    console.log('%s', /api/quotes/' + id);
+    console.log('%s', '/api/quotes/' + id);
     $http.get('/api/quotes/' + id).success(function(quote)
     {
     	$scope.quote = quote;
-    	console.log('%s', /api/quotes/' + id);
+    	console.log('%s', '/api/quotes/' + id);
     })
   });
