8379d7b45b4420eeb6fa289bf0059203faefd6fb
Charles Censier <charles.censier@gmail.com>
2014-12-20 16:39:36 +0100

    #camarche

--- client/app/quoteList/quoteList.controller.js
+++ client/app/quoteList/quoteList.controller.js
@@ -1,30 +1,28 @@
 'use strict';
 
-angular.module('justThinkApp', ['ngTable'])
-  .controller('QuotelistCtrl', function ($scope, $http) {
+angular.module('justThinkApp')
+  .controller('QuotelistCtrl', function ($scope, $http, ngTableParams) {
     $scope.message = 'Hello';
-    var quoteList;
     $http.get('/api/quotes').success(function(quotes)
     {
-    	quoteList = quotes;
     	$scope.quotes = quotes;
-    });
 
-    $scope.tableQuotes = new ngTableParams({
+    	$scope.tableQuotes = new ngTableParams({
         page: 1,            // show first page
         count: 10,          // count per page
         sorting: {
             name: 'asc'     // initial sorting
         }
     }, {
-        total: quoteList.length, // length of data
+        total: quotes.length, // length of data
         getData: function($defer, params) {
             // use build-in angular filter
             var orderedData = params.sorting() ?
-                                $filter('orderBy')(quoteList, params.orderBy()) :
-                                quoteList;
+                                $filter('orderBy')(quotes, params.orderBy()) :
+                                quotes;
 
             $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
         }
+    });
     });
   });
