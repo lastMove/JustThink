b838dc498ab75fb692265df84cefb5981f4ed0ac
Charles Censier <charles.censier@gmail.com>
2014-12-20 16:15:51 +0100

    ngTable Error

--- client/app/quoteList/quoteList.controller.js
+++ client/app/quoteList/quoteList.controller.js
@@ -1,10 +1,30 @@
 'use strict';
 
-angular.module('justThinkApp')
+angular.module('justThinkApp', ['ngTable'])
   .controller('QuotelistCtrl', function ($scope, $http) {
     $scope.message = 'Hello';
+    var quoteList;
     $http.get('/api/quotes').success(function(quotes)
     {
+    	quoteList = quotes;
     	$scope.quotes = quotes;
-    })
+    });
+
+    $scope.tableQuotes = new ngTableParams({
+        page: 1,            // show first page
+        count: 10,          // count per page
+        sorting: {
+            name: 'asc'     // initial sorting
+        }
+    }, {
+        total: quoteList.length, // length of data
+        getData: function($defer, params) {
+            // use build-in angular filter
+            var orderedData = params.sorting() ?
+                                $filter('orderBy')(quoteList, params.orderBy()) :
+                                quoteList;
+
+            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
   });
