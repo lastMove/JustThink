d2d49aace07d244d4bf0090a957ef9080b86d79e
Charles Censier <charles.censier@gmail.com>
2014-12-20 17:27:43 +0100

    #filter

--- client/app/quoteList/quoteList.controller.js
+++ client/app/quoteList/quoteList.controller.js
@@ -9,21 +9,25 @@ angular.module('justThinkApp')
     	$scope.quotes = quotes;
 
     	$scope.tableQuotes = new ngTableParams({
-        page: 1,            // show first page
-        count: 10,          // count per page
-        sorting: {
-            author: 'desc'     // initial sorting
-        }
-    }, {
-        total: quotes.length, // length of data
-        getData: function($defer, params) {
-            // use build-in angular filter
-            var orderedData = params.sorting() ?
-                                $filter('orderBy')(quotes, params.orderBy()) :
-                                quotes;
-
-            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-        }
-    });
+		    page: 1,            // show first page
+		    count: 10,          // count per page
+		    sorting: {
+		        date: 'desc'     // initial sorting
+		    }
+		}, {
+		    total: quotes.length, // length of data
+		    getData: function($defer, params) {
+		        // use build-in angular filter
+		        var filteredData = params.filter() ?
+				                    $filter('filter')(quotes, params.filter()) :
+				                    quotes;
+		        var orderedData = params.sorting() ?
+		                            $filter('orderBy')(filteredData, params.orderBy()) :
+		                            filteredData;
+		       $scope.quotes = orderedData;
+		       params.total(orderedData.length);
+		       $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+		    }
+		});
     });
   });
